64d4e720a9de681aecaa62dd66927c9d73f79f2a
Harold Martinez <hfmartinezc@ufpso.edu.co>
2022-10-06 19:32:31 -0500

    Se hace la conexion a la base de datos y se completa la clase Article

--- src/main/java/com/primer_parcial8/Models/Article.java
+++ src/main/java/com/primer_parcial8/Models/Article.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -13,6 +14,26 @@ public class Article {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long codigo;
+    private Long code
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @private Date dateRegister;
+
+    @Column(length = 200, nullable = false)
+    private String category;
+
+    @Column(length = 1000, nullable = false)
+    private int stock;
+
+    @Column(nullable = false)
+    private Float priceSale;
+
+    @Column(nullable = false)
+    private Float priceBuy;
 
 }
