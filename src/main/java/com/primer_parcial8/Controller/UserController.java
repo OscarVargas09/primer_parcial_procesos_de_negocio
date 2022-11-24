b094490c03e57767e9b0a245d6ed066aabbd4fd5
OscarVargasTejeda <oscarvargas@wposs.com>
2022-11-24 17:06:24 -0500

    Se realiza ajuste para el controller de user

--- src/main/java/com/primer_parcial8/Controller/UserController.java
+++ src/main/java/com/primer_parcial8/Controller/UserController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 public class UserController {
     @Autowired
     private UserService userService;
